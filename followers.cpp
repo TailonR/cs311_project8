305807c0cc37c3dcb302724b2dcd13c05b01843f
Tailon <renewedT2016@gmail.com>
2019-12-01 12:26:13 -0900

    Documentatino; adding docs to followers.cpp

--- followers.cpp
+++ followers.cpp
@@ -23,6 +23,11 @@
 #include <list>
 //for std::list
 
+//getName()
+//DESC: Gets input from user and assigns it to the parameter
+//PRE: Filename needs to be a valid string
+//Requirements on types: None
+//ERROR: No-except Guarantee
 void getName(std::string & filename)
 {
 	std::cout << "Enter a filename" << std::endl;
@@ -30,6 +35,11 @@ void getName(std::string & filename)
 	std::cout << std::endl;
 }
 
+//cleanInput()
+//DESC: Given a string, it checks if the the string is the name of an executable
+//PRE: Filname needs to be a valid string
+//Requirements on types: None
+//Error: Strong Guarantee
 bool cleanInput(std::string & filename)
 {
 	if (filename.substr(filename.size() - 3, filename.size() - 1) != "exe")
@@ -42,24 +52,54 @@ bool cleanInput(std::string & filename)
 	}
 }
 
+//sortWords
+//DESC: Given a vector and map, it adds sorts the vector in lexicographical order
+//PRE: vectOfFirstWords needs to be a valid vector, and tableOfWords needs to be a valid map
+//Requirement on types: vectOfFirstWords needs to be a vector of strings, and map needs to be a map of strings and lists of strings
+//ERROR: Strong Guarantee
 void sortWords(std::vector<std::string> & vectOfFirstWords, const std::map<std::string,std::list<std::string>> & tableOfWords)
 {
-    for (auto i : tableOfWords)
-    {
-        vectOfFirstWords.push_back(i.first);
-    }
-    std::sort(vectOfFirstWords.begin(), vectOfFirstWords.end());
+	try 
+	{
+		for (auto i : tableOfWords)
+		{
+			vectOfFirstWords.push_back(i.first);
+		}
+		std::sort(vectOfFirstWords.begin(), vectOfFirstWords.end());
+	}
+	catch (...)
+	{
+		throw;
+	}
 }
 
+//sortList()
+//DESC: Given a string, a vector of strings, and a map it sorts the words in the list 
+//PRE: i needs to be a valid string, vecOfListElemetns needs to be a valid vector, and tableOfWords needs to be a valid map
+//Requirements on types: vectOfListElements needs to be a vector strings and tableOfWords needs to be a map of strings and lists
+//ERROR: Strong Guarantee
 void sortList(const std::string & i, std::vector<std::string> & vectOfListElements, const std::map<std::string,std::list<std::string>> & tableOfWords)
 {
-    for (auto j : tableOfWords.find(i)->second)
-        {
-            vectOfListElements.push_back(j);
-        }
-        std::sort(vectOfListElements.begin(), vectOfListElements.end()); //sorting the values of the linked lists before printing them
+	try
+	{
+		for (auto j : tableOfWords.find(i)->second)
+		{
+			vectOfListElements.push_back(j);
+		}
+		std::sort(vectOfListElements.begin(), vectOfListElements.end()); //sorting the values of the linked lists before printing them
+	}
+	catch (...)
+	{
+		throw;
+	}
 }
 
+//printWords()
+//DESC: Given a vector of, a map of strings and lists, and an ostream prints the number of distinct words 
+//	and each word followed by the words that follow it 
+//PRE: vectOfWords needs to be a valid vector of strings, tableOfWords needs to be a valid map, and os needs to be a valid ostream
+//Requirements on types: vectorOfWords needs to be a vector of strings, tableOfWords needs to be a map of strings and lists
+//ERROR: Strong Error
 void printWords(std::vector<std::string> & vectOfFirstWords, const std::map<std::string,std::list<std::string>> & tableOfWords, std::ostream & os)
 {
 	os << "Number of distinct words: " << tableOfWords.size() << std::endl;
@@ -67,7 +107,16 @@ void printWords(std::vector<std::string> & vectOfFirstWords, const std::map<std:
 	for (auto i : vectOfFirstWords)
 	{
 		std::vector<std::string> vectOfListElements;
-        sortList(i,vectOfListElements,tableOfWords);
+
+		try
+		{
+			sortList(i,vectOfListElements,tableOfWords);
+		}
+		catch (...)
+		{
+			throw;
+		}
+
 		std::cout << i << ": ";
 		for (auto k : vectOfListElements)
 		{
@@ -77,7 +126,13 @@ void printWords(std::vector<std::string> & vectOfFirstWords, const std::map<std:
 	}
 }
 
-int numberOfAppearances(const std::string & prevWord, const std::map<std::string,std::list<std::string>> & tableOfWords, const std::string & word)
+//numberOfAppearances()
+//DESC: Given two strings, a map, and a string, looks to see if the second string is in the list attributed to the first string,
+//	and returns the number of times the word appears
+//PRE: prevWord  and word needs to be valid strings and tableOfWords needs to be a valid map
+//Requirements on types: tableOfWords needs to be a map of strings and lists
+//ERROR: NO-throw (or None?)
+int numberOfAppearances(const std::string & prevWord, const std::string& word, const std::map<std::string,std::list<std::string>> & tableOfWords)
 {
 	int numOfAppearances = 0;
 	for (auto i : tableOfWords.find(prevWord)->second) 
@@ -90,6 +145,12 @@ int numberOfAppearances(const std::string & prevWord, const std::map<std::string
 	return numOfAppearances;
 }
 
+//insertWord()
+//DESC: Given two strings, and a map, inserts a inserts one of the strings into the list
+//	asscociated with the other string
+//PRE: word and prevWord need to be valid strings, and tableOfWords needs to be a valid map
+//Requirements on types: tableOfWords needs to be a map of strings and lists
+//ERROR: Strong Guarantee
 void insertWord(std::string & word, std::string & prevWord, std::map<std::string,std::list<std::string>> & tableOfWords)
 {
     std::list<std::string> list;
@@ -111,7 +172,7 @@ void insertWord(std::string & word, std::string & prevWord, std::map<std::string
     else if (tableOfWords.find(prevWord) != tableOfWords.end()) //if prevWord is found
     {
         
-        if (numberOfAppearances(prevWord, tableOfWords, word) == 0)
+        if (numberOfAppearances(prevWord, word, tableOfWords) == 0)
         {
             tableOfWords.find(prevWord)->second.push_back(word); //only insert the prevWord and current word once if they are the same 
         }
@@ -119,6 +180,14 @@ void insertWord(std::string & word, std::string & prevWord, std::map<std::string
     prevWord = word;
 }
 
+//insertWords()
+//DESC: Given two strings, an ifstream, a map, and a list, it reads from the ifstream 
+//	and then inserts one of the strings and the list into the map 
+//PRE: word and prevWord need to be valid strings, fileReader needs to be a valid ifstream, 
+//	tableOfWords needs to be a valid map, and followingWordList needs to be a valid list
+//Requirements on types: tableOfWords needs to be a map of lists and strings, 
+//	and followingWordsList needs to be a list of strings
+//ERROR: Strong Guarantee
 void insertWords(std::string & word, std::string & prevWord, std::ifstream & fileReader,std::map<std::string,
 					 std::list<std::string>> & tableOfWords, std::list<std::string> & followingWordsList)
 {
